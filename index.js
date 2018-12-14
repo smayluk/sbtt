9897b4ec6c83325911786e96e4ac446e1699f33f
vzaychuk <Smayluk@gmail.com>
2018-12-14 12:17:05 +0300

    quick solution

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 var containerElem = document.getElementById('container');
-
+var zindex = 0;
 
 document.onmousedown = function(e) {
 	var rectangleElem = e.target;
 
 	if (!rectangleElem.classList.contains('draggable')) return;
-
+	rectangleElem.style.zIndex = ++zindex;
 
 	var rectangleCoords = getCoords(rectangleElem);
 	var shiftX = e.pageX - rectangleCoords.left;
 	var shiftY = e.pageY - rectangleCoords.top;
-
 
 	var containerCoords = getCoords(containerElem);
 
@@ -20,13 +19,13 @@ document.onmousedown = function(e) {
 	}
 
 	document.onmousemove = function(e) {
-		//  вычесть координату родителя, т.к. position: relative
 		var newLeft = e.pageX - shiftX - containerCoords.left;
 		var newTop = e.pageY - shiftY - containerCoords.top;
 
 		if (newLeft < 0) {
 			newLeft = 0;
 		}
+
 		var rightEdge = containerElem.offsetWidth - rectangleElem.offsetWidth;
 		if (newLeft > rightEdge) {
 			newLeft = rightEdge;
@@ -35,27 +34,25 @@ document.onmousedown = function(e) {
 		if (newTop < 0) {
 			newTop = 0;
 		}
+
 		var topEdge = containerElem.offsetHeight - rectangleElem.offsetHeight;
 		if (newTop > topEdge) {
 			newTop = topEdge;
 		}
 
-
 		rectangleElem.style.left = newLeft + 'px';
 		rectangleElem.style.top = newTop + 'px';
-
 	};
 
 	document.onmouseup = function() {
 		document.onmousemove = document.onmouseup = null;
 	};
-
 	return false;
 }
 
 
 
-function getCoords(elem) {   // кроме IE8-
+function getCoords(elem) {
 	var box = elem.getBoundingClientRect();
 	return {
 		top: box.top + pageYOffset,
